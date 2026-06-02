1f4ba6e78f4fd8f638ca154019a421b353509e0a
Russell-Huang <thefightofwolf@gmail.com>
2026-06-03 01:46:13 +0800

    0

--- main.js
+++ main.js
@@ -823,7 +823,13 @@ function executeBatchSubmit(list, baseData, btn, output) {
   btn.disabled = true;
   // 如果 1 人就不顯示人數
   const btnText = list.length > 1 ? `提交中 (${list.length}人)...` : "提交中...";
-  btn.innerText = btnText;
+  //btn.innerText = btnText;
+  btn.innerHTML = `
+      <svg class="fly-out-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="1.4em" height="1.4em" style="vertical-align: middle; margin-right: 8px;">
+        <path fill="white" d="M125.4 128C91.5 128 64 155.5 64 189.4C64 190.3 64 191.1 64.1 192L64 192L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 192L575.9 192C575.9 191.1 576 190.3 576 189.4C576 155.5 548.5 128 514.6 128L125.4 128zM528 256.3L528 448C528 456.8 520.8 464 512 464L128 464C119.2 464 112 456.8 112 448L112 256.3L266.8 373.7C298.2 397.6 341.7 397.6 373.2 373.7L528 256.3zM112 189.4C112 182 118 176 125.4 176L514.6 176C522 176 528 182 528 189.4C528 193.6 526 197.6 522.7 200.1L344.2 335.5C329.9 346.3 310.1 346.3 295.8 335.5L117.3 200.1C114 197.6 112 193.6 112 189.4z"/>
+      </svg>
+    `;
+    //<!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
 
   output.style.color = "#34495e";
   output.innerText = "正在處理報名資料，請稍候...";
