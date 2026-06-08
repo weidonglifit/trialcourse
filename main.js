5b5d274afa508925cf46ecd0e2470d95fff540f2
Russell-Huang <thefightofwolf@gmail.com>
2026-06-08 17:37:29 +0800

    0

--- main.js
+++ main.js
@@ -4982,7 +4982,7 @@ function closeOverlayAndAnimateLogo() {
 
       // 2. 寬度設定為 100%，但最大不超過原始圖片的寬度
       logo.style.width = '100%';
-      logo.style.maxWidth = targetRect.width + 'px';
+      logo.style.maxWidth = '352px';
       
       // 3. 鎖定長寬比例，讓高度跟著寬度一起縮放，絕對不變形
       logo.style.height = 'auto';
