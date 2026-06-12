834e86c2992252e0695cfa0541cf67ce9e185122
Russell-Huang <thefightofwolf@gmail.com>
2026-06-13 00:14:59 +0800

    0

--- main.js
+++ main.js
@@ -5268,7 +5268,7 @@ function searchHistoricalData() {
   // 初始化狀態
   output.style.display = "none";
   const originalText = btn.innerText;
-  btn.innerHTML = "搜尋中...";
+  btn.innerHTML = '<svg class="walking-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="16" height="16"><path fill="white" d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"/></svg>';
   btn.disabled = true;
 
   callGasApi("queryHistoricalData", [phone])
@@ -5318,9 +5318,6 @@ function renderHistoryCards(dataList) {
           <b style="color: #E87A90;">電話：</b>${item.phone}<br>
           <b style="color: #E87A90;">LINE：</b>${item.lineId}<br>
           <b style="color: #E87A90;">信箱：</b>${item.email}
-        </div>
-        <div style="text-align: right; margin-top: 10px;">
-          <span style="background: #E87A90; color: white; padding: 6px 14px; border-radius: 20px; font-size: 0.9em; font-weight: bold;">帶入此資料</span>
         </div>
       </div>
     `;
